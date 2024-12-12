c45062b41c94a8ad3e77d2a8e1e241ccb6ece647
mrsen <muhammetrasitsen@gmail.com>
2024-12-12 13:28:42 -0800

    changed yml

--- SQLite-Sharp/Program.cs
+++ SQLite-Sharp/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string? userName = Console.ReadLine();
+
+if (string.IsNullOrEmpty(userName)){
+    Console.WriteLine("I Dont know who you are!");
+}else{
+    Console.WriteLine("Ohhh Hello " + userName);
+}
+
+Thread.Sleep(10000);
